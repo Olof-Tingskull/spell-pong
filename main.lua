ad091af1d47bd1172d46e4010e294027a58585fd
Gustav Nilsson <gustavn64@gmail.com>
2013-08-15 17:36:23 +0200

    Make the ball bounce vertically

--- main.lua
+++ main.lua
@@ -2,10 +2,17 @@ function love.load()
   ball = {}
   ball.x = 400
   ball.y = 300
+  ball.dx = 400
+  ball.dy = 400
 end
 
 function love.update(dt)
+  ball.x = ball.x + ball.dx * dt
+  ball.y = ball.y + ball.dy * dt
 
+  if (ball.y - 15 < 0 and ball.dy < 0) or (ball.y + 15 > 600 and ball.dy > 0) then
+    ball.dy = -ball.dy
+  end
 end
 
 function love.draw()
