a50ffe66ee906630ae5c974e16edae9a1b0e58e4
Olof-Tingskull <olof.tingskull@telia.com>
2013-08-15 18:27:19 +0200

    add computer steering

--- main.lua
+++ main.lua
@@ -3,12 +3,20 @@ function love.load()
   ball.x = 400
   ball.y = 300
   rightpaddle = {}
-  rightpaddle.y = 230
+  rightpaddle.y = 0
   leftpaddle = {}
-  leftpaddle.y =230
+  leftpaddle.y = 300
 end
 
 function love.update(dt)
+  if ball.x > 400 then
+  if ball.y > rightpaddle.y then
+    rightpaddle.y = rightpaddle.y + 200 *dt
+  end
+  if ball.y < rightpaddle.y then
+    rightpaddle.y = rightpaddle.y - 200 *dt
+  end
+  end
   if love.keyboard.isDown("down") then
     leftpaddle.y = leftpaddle.y + 200 *dt
   end
@@ -18,7 +26,8 @@ function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.line (400, 0, 400, 600)
   love.graphics.circle("fill", ball.x, ball.y, 15, 20)
-  love.graphics.rectangle ("fill", 780, rightpaddle.y, 20, 140)
-  love.graphics.rectangle ("fill", 0, leftpaddle.y, 20, 140)
+  love.graphics.rectangle ("fill", 780, rightpaddle.y - 70, 20, 140)
+  love.graphics.rectangle ("fill", 0, leftpaddle.y - 70, 20, 140)
 end
