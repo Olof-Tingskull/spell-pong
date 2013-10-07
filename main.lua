6184df3f226541f6ff1c9f7029406b572d548832
Olof-Tingskull <olof.tingskull@telia.com>
2013-10-07 20:00:47 +0200

    add points to the players

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@ function love.load()
   ball.y = 300
   ball.dx = 400
   ball.dy = 400
+  leftpoints = 0
+  rightpoints = 0
 
   rightpaddle = {}
   rightpaddle.y = 300
@@ -23,9 +25,20 @@ function love.update(dt)
     ball.dx = -ball.dx
   end
 
+  
   if ball.x > 765 and ball.dx > 0 and ball.y + 15 > rightpaddle.y - 70 and ball.y - 15 < rightpaddle.y + 70 then
     ball.dx = -ball.dx
   end
+
+    if ball.x > 785 and ball.dx > 0 then
+    ball.dx = -ball.dx
+    leftpoints = leftpoints + 1
+  end
+    if ball.x < 15 and ball.dx < 0 then
+    ball.dx = -ball.dx
+    rightpoints = rightpoints + 1
+  end
+  
 
   if ball.x > 400 then
     if ball.y > rightpaddle.y then
@@ -52,6 +65,8 @@ function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.print (leftpoints, 0, 0)
+  love.graphics.print (rightpoints, 770, 0)
   love.graphics.line (400, 0, 400, 600)
   love.graphics.circle("fill", ball.x, ball.y, 15, 20)
   love.graphics.rectangle("fill", 780, rightpaddle.y - 70, 20, 140)
